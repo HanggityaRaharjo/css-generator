ee80a160cd691c2ca53adc2d1b808db12a6f583c
Hanggitya slice <hanggityaslice@gmail.com>
2023-04-10 16:49:30 +0700

    update

---
+++ backend/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+   
+    $myfile = fopen("newfile.css", "w") or die("Unable to open file!");
+  
+    fwrite($myfile, '*{');
+    fwrite($myfile, 'border:1px solid red;}');
+
+
+
+
+    
+    fclose($myfile);
+
+    return view('welcome');
+});
